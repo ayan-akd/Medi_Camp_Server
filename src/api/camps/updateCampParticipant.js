4c3c4f0ac25acde89dfec8f66e289fbf476cd8dd
akd420 <ayanakd112@gmail.com>
2023-11-26 22:10:19 +0600

    fixed the put operation

--- src/api/camps/updateCampParticipant.js
+++ src/api/camps/updateCampParticipant.js
@@ -1,13 +1,21 @@
 const Camps = require("../../models/Camps");
 const updateCampParticipant = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const camp = await Camps.findById(id);
-        const { participants } = camp;
-        const result = await Camps.findByIdAndUpdate(id, { participants }, { new: true });
-        res.status(200).send({ message: "Participant added successfully", result });
-    } catch (error) {
-        next(error);
+  try {
+    console.log(req.params.id);
+    const { id } = req.params;
+    console.log(id);
+    const camp = await Camps.findById(id);
+    if (!camp) {
+      return res.status(404).send({ message: "Camp not found" });
     }
-}
+    const result = await Camps.findByIdAndUpdate(
+      id,
+      { participants: req.body.participants },
+      { new: true }
+    );
+    res.status(200).send({ message: "Participant added successfully", result });
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = updateCampParticipant;
--- src/routes/camps/index.js
+++ src/routes/camps/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var addCamp = require("../../api/camps/addCamps");
 var getCamps = require("../../api/camps/getCamps");
 var getCampById = require("../../api/camps/getCampById");
-var updateCampParticipants = require("../../api/camps/updateCampParticipants");
+var updateCampParticipants = require("../../api/camps/updateCampParticipant");
 
 router.post("/camps", addCamp);
 router.get("/camps", getCamps);
