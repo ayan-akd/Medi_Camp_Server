23432e1660e66f21957d8d8badf79f283ea5fbc8
akd420 <ayanakd112@gmail.com>
2023-11-27 22:07:30 +0600

    added delete camp operation

---
+++ src/api/camps/deleteCamp.js
@@ -0,0 +1,15 @@
+const Camp = require("../../models/Camps");
+
+const deleteCamp = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const camp = await Camp.findByIdAndDelete(id);
+    if (!camp) {
+      return res.status(404).send({ message: "Camp not found" });
+    }
+    res.status(200).send({ message: "Camp deleted successfully", camp });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = deleteCamp;
--- src/routes/camps/index.js
+++ src/routes/camps/index.js
@@ -5,11 +5,14 @@ var getCamps = require("../../api/camps/getCamps");
 var getCampById = require("../../api/camps/getCampById");
 var updateCampParticipants = require("../../api/camps/updateCampParticipant");
 var updateCamp = require("../../api/camps/updateCamp");
+const deleteCamp = require("../../api/camps/deleteCamp");
+
 
 router.post("/camps", addCamp);
 router.get("/camps", getCamps);
 router.get("/camps/:id", getCampById);
 router.put("/camps/:id", updateCampParticipants);
 router.put("/camp/:id", updateCamp);
+router.delete("/camp/:id", deleteCamp);
 
 module.exports = router;
