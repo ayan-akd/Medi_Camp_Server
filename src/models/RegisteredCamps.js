4fbe26a3d7585480e6b7ec1f7745b5e499b51d63
akd420 <ayanakd112@gmail.com>
2023-11-28 13:06:59 +0600

    updated the model for registered camps

--- src/models/RegisteredCamps.js
+++ src/models/RegisteredCamps.js
@@ -1,63 +1,95 @@
 const { Schema, model } = require("mongoose");
 
 const registeredCampSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        age: {
-            type: Number,
-            required: true,
-        },
-        gender: {
-            type: String,
-            required: true,
-        },
-        fee: {
-            type: Number,
-            required: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        problems: {
-            type: String,
-            required: true,
-        },
-        emergency: {
-            type: String,
-            required: true,
-        },
-        hostEmail: {
-            type: String,
-            required: true,
-        },
-        campId: {
-            type: String,
-            required: true,
-        },
-        payment: {
-            type: String,
-            required: true,
-        },
-        confirmation: {
-            type: String,
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        collection: "registeredCamps",
-    }
+    email: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    fee: {
+      type: Number,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    problems: {
+      type: String,
+      required: true,
+    },
+    emergency: {
+      type: String,
+      required: true,
+    },
+    hostEmail: {
+      type: String,
+      required: true,
+    },
+    campId: {
+      type: String,
+      required: true,
+    },
+    payment: {
+      type: String,
+      required: true,
+    },
+    confirmation: {
+      type: String,
+      required: true,
+    },
+    campName: {
+      type: String,
+      required: true,
+    },
+    targetAudience: {
+      type: String,
+      required: true,
+    },
+    services: {
+      type: String,
+      required: true,
+    },
+    professionals: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: "registeredCamps",
+  }
 );
 const registeredCamps = model("registeredCamps", registeredCampSchema);
 module.exports = registeredCamps;
